7d8e4d68ac7859b8b8091cebbe76e6f8c419ec8b
Alex Kruglow <alexkruglow@gmail.com>
2022-10-19 23:20:46 +0300

    Init commit

---
+++ python_trainee_assignment.py
@@ -0,0 +1,14 @@
+async def get_matrix(url: str) -> list[int]:
+    pass
+
+
+async def _get_data_by_url(url: str) -> str:
+    pass
+
+
+def _convert_str_matrix_to_list_matrix(str_matrix: str) -> list[list[int]]:
+    pass
+
+
+def _pass_matrix_by_spiral(matrix: list[list[int]]) -> list[int]:
+    pass
