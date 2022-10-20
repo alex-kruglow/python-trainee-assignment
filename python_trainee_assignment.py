bce3886e0dd94cbc7b155a3ed0a70b77adc7feed
Alex Kruglow <alexkruglow@gmail.com>
2022-10-20 21:39:45 +0300

    _get_data_by_url was realized.

--- python_trainee_assignment.py
+++ python_trainee_assignment.py
@@ -1,9 +1,19 @@
+import aiohttp
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 async def get_matrix(url: str) -> list[int]:
     pass
 
 
-async def _get_data_by_url(url: str) -> str:
-    pass
+async def _get_data_by_url(url: str) -> str | None:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                text: str = await resp.text()
+    return text
 
 
 def _convert_str_matrix_to_list_matrix(str_matrix: str) -> list[list[int]]:
